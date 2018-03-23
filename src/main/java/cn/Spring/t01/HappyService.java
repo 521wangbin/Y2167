a87a9d13e1b3351aab1bf74f8e8c1c35a5483df4
LEOMENG\丶萌 <1172987904@qq.com>
2018-03-23 16:22:47 +0800

    Spring

---
+++ src/main/java/cn/Spring/t01/HappyService.java
@@ -0,0 +1,25 @@
+package cn.Spring.t01;
+
+/**
+ * Created by 丶萌 on 2018/3/3.
+ */
+public class HappyService {
+    private String name;
+    private Integer age;
+    private String authorss;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
